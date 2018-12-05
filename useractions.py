5787a3fcc68daacb02e8e8d51932f8c19e32c72b
Tomer Ben Levi <36502722+tomerbl123@users.noreply.github.com>
2018-12-05 22:38:56 +0200

    Add files via upload
    The BE's first file.

---
+++ useractions.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def insertdb():
+
+
+@app.route('/')
+def updatedb():
+
+
+@app.route('/')
+def deletefromdb():
+
+
+if __name__=='__main__':
+    app.run(debug=True)
