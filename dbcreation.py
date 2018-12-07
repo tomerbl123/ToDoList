2a591f088f777c2f8c61f1fbf9d707de4e0e591d
Tomer Ben Levi <36502722+tomerbl123@users.noreply.github.com>
2018-12-07 18:54:23 +0200

    Add files via upload
    Database Additions + Started to work on the AddTask function

--- dbcreation.py
+++ dbcreation.py
@@ -10,10 +10,12 @@ class TasksList(Base):
 	__tablename__='TasksList'
 
 	id=Column(Integer, primary_key=True)
-	task=Column(String(80), nullable=False)
+	task=Column(String(200), nullable=False)
+	isdone=Column(String(1), nullable=True)
 
-	def __init__(self, task=None):
+	def __init__(self, task=None, isdone='0'):
 		self.task=task
+		self.isdone=isdone
 
 engine = create_engine('sqlite:////Users/Tomer Ben-Levi/Projects/ToDoList/example.db')
 Base.metadata.create_all(bind=engine)
--- useractions.py
+++ useractions.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-import dbcreation
+#import dbcreation
 from dbcreation import TasksList, session
 
 app=Flask('__name__')
@@ -9,9 +9,12 @@ app=Flask('__name__')
 def hello():
 	return("hello world")
 
-NewTask = TasksList('tomer')
-session.add(NewTask)
-session.commit()
+def addtasktodb(task):
+	NewTask = TasksList(task)
+	session.add(NewTask)
+	session.commit()
+	#why it adds the items twice?? what the TasksList class really does?
+addtasktodb('something')
 
 if __name__=='__main__':
     app.run(debug=True)
