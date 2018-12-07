e072b2546d22e08b37cdb04b92d176aa4b39c7e5
Tomer Ben Levi <36502722+tomerbl123@users.noreply.github.com>
2018-12-07 16:46:42 +0200

    Add files via upload
    Added database creation class using SQLAlchemy.
    The database is Sqlite.

--- useractions.py
+++ useractions.py
@@ -1,21 +1,36 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-import sqlite3
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relation, sessionmaker
 
-app = Flask(__name__)
+app=Flask('__name__')
 
-@app.route('/')
-def inserttodb():
+@app.route('/homepage')
+def hello():
+	return("hello world")
+	
+Base = declarative_base()
+
+class TasksList(Base):
+	__tablename__='TasksList'
+
+	id=Column(Integer, primary_key=True)
+	task=Column(String(80), nullable=False)
+
+	def __init__(self, task=None):
+		self.task=task
 
 
-@app.route('/')
-def updatetodb():
+engine = create_engine('sqlite:////Users/Tomer Ben-Levi/Projects/ToDoList/example.db')
+Base.metadata.create_all(bind=engine)
 
+Session = sessionmaker(bind=engine)
+session = Session()
 
-@app.route('/')
-def deletefromdb():
-
+NewTask = TasksList("Robocop")
+session.add(NewTask)
+session.commit()
 
 if __name__=='__main__':
     app.run(debug=True)
