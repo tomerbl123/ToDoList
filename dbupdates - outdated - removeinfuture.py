da3b2ded327d4906723baff821965d7fa60e5890
Tomer Ben Levi <36502722+tomerbl123@users.noreply.github.com>
2018-12-15 21:54:48 +0200

    Add files via upload

---
+++ dbupdates - outdated - removeinfuture.py
@@ -0,0 +1,26 @@
+from flaskapp import TasksList, db
+
+def addtask(class_name, task, is_done='0'):
+	NewTask = class_name(task, is_done)
+	db.session.add(NewTask)
+	db.session.commit()
+
+def removetask(class_name, task_id):
+	q = db.session.query(class_name).filter_by(id=task_id)
+	q.delete()
+	db.session.commit()
+
+def updatetask(class_name, task_id, newtask):
+	q = db.session.query(class_name).filter_by(id=task_id)
+	q.update({class_name.task: newtask})
+	db.session.commit()
+
+def getalltasks(class_name):
+	q = db.session.query(class_name.task, class_name.isdone).all()
+	print(q)
+
+
+#addtask(TasksList,'asd','1')
+#removetask(TasksList, 1)
+#updatetask(TasksList, 2, 'asdasd')
+#getalltasks(TasksList)
