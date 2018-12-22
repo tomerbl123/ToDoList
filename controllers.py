5ed43413c1dc48cfd501e6ccfdd117c9504d6452
Tomer Ben Levi <36502722+tomerbl123@users.noreply.github.com>
2018-12-22 22:24:13 +0200

    Fixing no_tasks bug
    If we had 0/1 tasks before this change the page failed to load (ran into edge case in the code).
    
    Here we refactored the creating_table_rows_string function so it can now deal with all these edge_cases.

--- controllers.py
+++ controllers.py
@@ -104,14 +104,7 @@ def static_html_string():
 				</fieldset>
 			</form>
 
-				<br><br>
-				<h2>What should I do?</h2>
-
-				<table style="width:100%">
-				  <tr>
-				    <th class='id'>Number</th>
-				    <th class='description'>Description</th>
-				  </tr>
+			<br><br>
 	"""
 	return html_static_part
 				  
@@ -120,30 +113,49 @@ def creating_table_rows_string(data_base, class_name):
 	This function creates the dynamic part of the HTML String.
 	Meaning, it creates table rows according to the amount of tasks that we have.
 	"""
-	data = data_base.session.query(class_name.id, class_name.task, class_name.isdone).order_by(class_name.isdone).all()
-	tasks_list=[]
-	counter=0
-	for item in data:
-		if item[2] == '0':
-			text = "<tr><td>{0}</td><td>{1}</td></tr>".format(item[0], item[1])
-			tasks_list.append(text)
-			if data[counter+1][2]:
-				tasks_list.append("""</table><br><h2>Done.</h2><table style="width:100%">
-					<tr><th class='id'>Number</th><th class='description'>Description</th></tr>""")
-		else:
-			text = "<tr><td>{0}</td><td>{1}</td></tr>".format(item[0], item[1])
-			tasks_list.append(text)
-		counter+=1
+
+	#Querying the amount of tasks that we have (done and not done).
+	amount_of_not_done_tasks = data_base.session.query(class_name.id, class_name.task, class_name.isdone).filter_by(isdone='0').count()
+	amount_if_done_tasks = data_base.session.query(class_name.id, class_name.task, class_name.isdone).filter_by(isdone='1').count()
+	
+	done_tasks_list=[]
+	not_done_tasks_list=[]
 	html_changing_part = ''
-	for item in tasks_list:
+
+	#Creating the NOT Done tasks' table (no tasks -> no table).
+	if amount_of_not_done_tasks > 0:
+		data_not_done = data_base.session.query(class_name.id, class_name.task, class_name.isdone).filter_by(isdone='0')
+		not_done_tasks_list.append("""<h2>What should I do?</h2><table style="width:100%"><tr><th>Number</th><th>Description</th></tr>""")
+		for item in data_not_done:
+			text = "<tr><td>{0}</td><td>{1}</td></tr>".format(item[0], item[1])
+			not_done_tasks_list.append(text)
+		not_done_tasks_list.append("""</table><br><br>""")
+	else:
+		not_done_tasks_list.append("""<h2>Nothing to do...</h2><br>""")
+
+	#Creating the Done tasks' table (no tasks -> no table).
+	if amount_if_done_tasks > 0:
+		data_done = data_base.session.query(class_name.id, class_name.task, class_name.isdone).filter_by(isdone='1')
+		done_tasks_list.append("""<h2>Done Tasks:</h2><table style="width:100%"><tr><th>Number</th><th>Description</th></tr>""")
+		for item in data_done:
+			text = "<tr><td>{0}</td><td>{1}</td></tr>".format(item[0], item[1])
+			done_tasks_list.append(text)
+		done_tasks_list.append("""</table>""")
+
+	#Creating the unified tasks (Done and NOT Done string).
+	for item in not_done_tasks_list:
 		html_changing_part += item
+
+	for item in done_tasks_list:
+		html_changing_part += item
+
 	return html_changing_part
 
 def create_full_html_string(html_static_part, html_changing_part):
 	"""
 	This fuction takes all the parts of the HTML String and combine them into one FULL HTML String.
 	"""
-	closer="""</table></body></html>"""
+	closer="""</body></html>"""
 	full_html = html_static_part + html_changing_part + closer
 	return full_html
 
