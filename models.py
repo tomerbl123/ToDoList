0b01bf6edb964b2dea384d5b2c9df550f1fb1fe3
Tomer Ben Levi <36502722+tomerbl123@users.noreply.github.com>
2018-12-28 20:55:28 +0200

    add_foreign_key_user_task
    Added foreign_key for user and task.
    In comment atm, due to issue with sessions and everything (who is the logged in user etc..).

--- models.py
+++ models.py
@@ -1,23 +1,5 @@
 from __init__ import app, db
 
-class Tasks(db.Model):
-	"""
-	This class creates the Tasks table in the DB.
-	There are 3 clomuns here: Task_ID(id), Task_Description(task) and a mark whether the task is done or not (isdone).
-	"""
-	__tablename__ = 'Tasks'
-
-	id = db.Column(db.Integer, primary_key = True)
-	task = db.Column(db.String(255), nullable = False)
-	isdone = db.Column(db.String(1), nullable = True)
-
-	def __init__(self, task=None, isdone='0'):
-		self.task=task
-		self.isdone=isdone
-	
-	def __repr__(self):
-		return 'The id is {}, Task is {}'.format(self.id, self.task)
-
 class User(db.Model):
 	"""
 	The users of the system.
@@ -29,7 +11,29 @@ class User(db.Model):
 	user_name = db.Column(db.String, nullable = False)
 	password = db.Column(db.String, nullable = False)
 
+	#db.relationship("Tasks")
+
 	def __init__(self, name=None, user_name=None, password=None):
 		self.name=name
 		self.user_name=user_name
 		self.password=password
+
+class Tasks(db.Model):
+	"""
+	This class creates the Tasks table in the DB.
+	There are 3 clomuns here: Task_ID(id), Task_Description(task) and a mark whether the task is done or not (isdone).
+	"""
+	__tablename__ = 'Tasks'
+
+	id = db.Column(db.Integer, primary_key = True)
+	task = db.Column(db.String(255), nullable = False)
+	isdone = db.Column(db.String(1), nullable = True)
+	#tasks_per_user = db.Column(db.Integer, db.ForeignKey('User.id'))
+
+	def __init__(self, task=None, isdone='0'):
+		self.task=task
+		self.isdone=isdone
+		#self.tasks_per_user=tasks_per_user
+	
+	def __repr__(self):
+		return 'The id is {}, Task is {}'.format(self.id, self.task)
