ac437370ad232369be7cba12cf103d2fed5d965b
Tomer Ben-Levi <tomerbl12@gmail.com>
2019-02-12 00:42:41 +0200

    change_uri

--- run.py
+++ run.py
@@ -5,9 +5,12 @@ app=Flask('__name__')
 
 app.config.update(
 	SECRET_KEY='topsecret',
-	SQLALCHEMY_DATABASE_URI='postgres://ienoldtxeklqyu:6791e4f04297cc8862dfd31697be0505bf64d8b64c6747ae17b69c149613a6b5@ec2-79-125-4-96.eu-west-1.compute.amazonaws.com:5432/dagofqd7saj392',
+	SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
 	SQLALCHEMY_TRACK_MODIFICATIONS=False,
     DEBUG = True,
 	use_reloader=False)
+
+#'postgres://ienoldtxeklqyu:6791e4f04297cc8862dfd31697be0505bf64d8b64c6747ae17b69c149613a6b5@ec2-79-125-4-96.eu-west-1.compute.amazonaws.com:5432/dagofqd7saj392',
+
 
 db=SQLAlchemy(app)
