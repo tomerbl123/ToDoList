f10ce05611e8855ea66bdf70f742e31c5dd8a53f
Tomer Ben-Levi <tomerbl12@gmail.com>
2019-02-11 23:52:31 +0200

    add_manage_py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app=Flask('__name__')
 
 app.config.update(
 	SECRET_KEY='topsecret',
-	SQLALCHEMY_DATABASE_URI='sqlite:////ToDoList/Tasks.db',
+	SQLALCHEMY_DATABASE_URI='postgres://ddbkzqemmhbsfe:473523aa042702f7e7b6e763df7e59600a7f4dcdfd4ea2280b075f6bc61d7939@ec2-54-217-208-105.eu-west-1.compute.amazonaws.com:5432/danqr65is0894t',
 	SQLALCHEMY_TRACK_MODIFICATIONS=False,
     DEBUG = True,
 	use_reloader=False)
