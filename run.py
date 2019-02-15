ba3600aa94e9991ea3c2403d6440ed5485a3381f
Tomer Ben-Levi <tomerbl12@gmail.com>
2019-02-15 20:54:30 +0200

    delete_hardcoded_db_url

--- run.py
+++ run.py
@@ -11,7 +11,4 @@ app.config.update(
     DEBUG = True,
 	use_reloader=False)
 
-#'postgres://ienoldtxeklqyu:6791e4f04297cc8862dfd31697be0505bf64d8b64c6747ae17b69c149613a6b5@ec2-79-125-4-96.eu-west-1.compute.amazonaws.com:5432/dagofqd7saj392',
-
-
 db=SQLAlchemy(app)
